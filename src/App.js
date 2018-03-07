90e691802d2c48ebc4b6e6941810a42fe93e5021
Alexandre Gonçalves <alexandre.dgoncalves@gmail.com>
2018-03-07 15:05:47 +0000

    05 - Move state upwards.

--- src/App.js
+++ src/App.js
@@ -1,40 +1,59 @@
 import React, { Component } from "react";
 
 class Square extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
-
   render() {
     return (
-      <button className="square" onClick={() => this.setState({ value: "X" })}>
-        {this.state.value}
+      <button className="square" onClick={() => this.props.onClick()}>
+        {this.props.value}
       </button>
     );
   }
 }
 
 class Board extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
+  handleSquareClick(position) {
+    const { squares } = this.state;
+    const nextSquares = [...squares];
+    nextSquares[position] = "X";
+    this.setState({
+      squares: nextSquares,
+    });
+  }
+
+  renderSquare(position) {
+    return (
+      <Square
+        position={position}
+        value={this.state.squares[position]}
+        onClick={() => this.handleSquareClick(position)}
+      />
+    );
+  }
+
   render() {
     return (
       <div>
         <div className="board-row">
-          <Square position={1} />
-          <Square position={2} />
-          <Square position={3} />
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
         </div>
         <div className="board-row">
-          <Square position={4} />
-          <Square position={5} />
-          <Square position={6} />
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
         </div>
         <div className="board-row">
-          <Square position={7} />
-          <Square position={8} />
-          <Square position={9} />
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
         </div>
       </div>
     );
