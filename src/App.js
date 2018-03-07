f0e833a11c0804dfdb9e642baa1e942901f48099
Alexandre Gonçalves <alexandre.dgoncalves@gmail.com>
2018-03-07 13:37:16 +0000

    02 - Add placeholder components.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-class App extends Component {
+class Square extends Component {
+  render() {
+    return <button className="square"></button>
+  }
+}
+
+class Board extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <div className="board-row">
+          <Square></Square>
+          <Square></Square>
+          <Square></Square>
+        </div>
+        <div className="board-row">
+          <Square></Square>
+          <Square></Square>
+          <Square></Square>
+        </div>
+        <div className="board-row">
+          <Square></Square>
+          <Square></Square>
+          <Square></Square>
+        </div>
+      </div>
+    )
+  }
+}
+
+class TicTacToe extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <Board/>
+        </div>
+        <div>
+          Next Player: X
+        </div>
       </div>
     );
   }
 }
 
-export default App;
+export default TicTacToe;
